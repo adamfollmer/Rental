d2d5fed68816bcf2d1000a2a0af9e9fe2f9385e1
Derek Scheller <derekscheller23@gmail.com>
2016-05-18 12:10:43 -0500

    Uppdate components

--- Rental2/src/Rental2/Components/TenantNav.cs
+++ Rental2/src/Rental2/Components/TenantNav.cs
@@ -3,51 +3,64 @@ using Rental2.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using System.Security.Claims;
-using System.Collections;
+using System.Threading.Tasks;
 
 namespace Rental2.Components
 {
+    [ViewComponent]
     public class TenantNav : ViewComponent
     {
         private ApplicationDbContext _context;
-        private ApplicationUser _user;
-        public TenantNav(ApplicationDbContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public TenantNav(ApplicationDbContext context,UserManager<ApplicationUser> userManager)
         {
             _context = context;
-            _user = (ApplicationUser)HttpContext.User.Identity;
+            _userManager = userManager;
         }
-        public  IViewComponentResult Invoke()
+        public IViewComponentResult Invoke()
         {
-            var navigationItems =  LoadTenantNavItems();
+            var navigationItems = LoadTenantNavItems();
             var viewModel = new ViewModel(navigationItems);
             return View(viewModel);
         }
-        public ItemViewModel LoadTenantNavItems()
+        public async Task<ApplicationUser> GetCurrentUser()
         {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            return user;
+        }
+        public Idefine LoadTenantNavItems()
+        {
+            var _user = GetCurrentUser().Result;
+            if(_context.RentalUserConnections.FirstOrDefault(x => x.ApplicationUserId == _user.Id) != null)
+            {
             RentalUserConnection connection = _context.RentalUserConnections.FirstOrDefault(x => x.ApplicationUserId == _user.Id);
             YearlyRental yearlyrental = _context.YearlyRentals.FirstOrDefault(x => x.ID == connection.YearlyRentalId);
-            ICollection<Bill> bills = (ICollection<Bill>)from t in _context.Bills where t.YearlyRentalId == yearlyrental.ID select t;
+            ICollection<Bill> bills = (ICollection<Bill>)_context.Bills.Where(x => x.YearlyRentalId == yearlyrental.ID).ToAsyncEnumerable();
             ICollection<Payment> payments = null;
             foreach (Bill _bill in bills)
             {
-                payments = (ICollection<Payment>)from t in _context.Payments where t.BillId == _bill.ID select t;
+                payments = (ICollection<Payment>)_context.Payments.Where(x => x.BillId == _bill.ID).ToAsyncEnumerable();
             }
             var amountDue = CalculateBalance(bills, payments);
-            Property property = _context.Properties.FirstOrDefault(x => x.ID == yearlyrental.PropertyID);
             var userData = new ItemViewModel()
             {
-                PropertyID = property.ID,
-                UserProperty = property,
-                UserId = _user.Id,
-                User = _user,
+                UserProperty = (Property)_context.Properties.Where(x => x.ID == yearlyrental.PropertyID),
+                CurrentUser = _user,
                 BalanceRemaining = amountDue,
-                BillId = bills.ElementAt(0).ID,
                 CurrentBill = bills.ElementAt(0)
             };
-            return userData;
+                return userData;
+            }
+            else
+            {
+                var userData = new BareViewModel()
+                {
+                    CurrentUser = _user
+                };
+                return userData;
+            }
         }
         public decimal CalculateBalance(ICollection<Bill> bills, ICollection<Payment> payments)
         {
@@ -68,20 +81,32 @@ namespace Rental2.Components
         public class ViewModel
         {
             public ItemViewModel NavigationItems { get; }
-            public ViewModel(ItemViewModel navigationItems)
+            public BareViewModel BaseNavItems { get; }
+            public ViewModel(Idefine navigationItems)
             {
-                NavigationItems = navigationItems;
+                if(navigationItems.CurrentUser.RentalHistory.Count != 0)
+                {
+                NavigationItems = (ItemViewModel)navigationItems;
+                }else
+                {
+                BaseNavItems = (BareViewModel)navigationItems;
+                }
             }
         }
-        public class ItemViewModel
+        public interface Idefine
+            {
+            ApplicationUser CurrentUser { get; set; }
+        }
+        public class ItemViewModel : Idefine
         {
-            public int PropertyID { get; set; }
             public Property UserProperty { get; set; }
-            public string UserId { get; set; }
-            public ApplicationUser User { get; set; }
-            public int BillId { get; set; }
+            public ApplicationUser CurrentUser { get; set; }
             public Bill CurrentBill { get; set; }
             public decimal BalanceRemaining { get; set; }
+        }
+        public class BareViewModel : Idefine
+        {
+            public ApplicationUser CurrentUser { get; set; }
         }
     }
 }
